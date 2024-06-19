2340c0304bbe70152491375916cd080c1b77f39b
manan kansara <mkansara0903@gmail.com>
2024-06-19 23:11:35 +0530

    changes

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,7 @@ import {
   requestBodySchema,
   loginrequestBodySchema,
 } from "@/utils/schema/auth.schema";
-export async function middleware(request: any) {
-  
+export async function middleware(request: NextRequest) {
   if (checkingPath.includes(request.nextUrl.pathname)) {
     const findIndex = checkingPath.findIndex(
       (path) => path === request.nextUrl.pathname
@@ -20,16 +19,27 @@ export async function middleware(request: any) {
         { status: 400 }
       );
     }
+  } else if (frontendPath.includes(request.nextUrl.pathname)) {
+    console.log(Object.keys(request.cookies));
+    if (Object.keys(request.cookies).length <= 2) {
+      const domain = request.headers.get("host");
+      const redirectUrl = `https://${domain}/auth`;
+      return NextResponse.redirect(redirectUrl);
+    }
+    console.log(request.cookies);
   }
-  else if(frontendPath.includes(request.nextUrl.pathname)){
-    
-  }
-  
+
   return NextResponse.next();
 }
 const checkingPath = ["/api/auth/signup", "/api/auth/signin"];
-const frontendPath = ["/user","/auth","/admin"]
+const frontendPath = ["/user", "/auth", "/admin", "/"];
 const validationSchema = [requestBodySchema, loginrequestBodySchema];
 export const config = {
-  matcher: ["/api/:path*","/auth/:path*","/user/:path*","/admin/:path*"],
+  matcher: [
+    "/api/:path*",
+    "/auth/:path*",
+    "/user/:path*",
+    "/admin/:path*",
+    "/",
+  ],
 };
